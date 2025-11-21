7d31435b539bced2cafac3b823476cd040e9ba26
Rikaruu <raihancoding7812@gmail.com>
2025-11-21 16:50:40 +0700

    Update Alpro.cpp

--- Alpro.cpp
+++ Alpro.cpp
@@ -2,24 +2,11 @@
 using namespace std;
 
 int main(){
-	int a,b;
-	cout << "Masukkan Jumlah Baris Matrik : ";cin >> a;
-	cout << "Masukkan Jumlah Kolom Matrik : ";cin >> b;
-	int matrik[a][b];
-	cout << "Masukkan nilai Matriks"<<endl;
-	for (int i=0;i < a; i++){
-		for (int j=0; j<b; j++){
-			cout <<"["<< i << "] " <<"["<< j<< "] = ";cin >> matrik[i][j];
-		}
-	}
-	cout << endl;
-	cout << "Tampilan Matriks"<<endl;
-	for (int k = 0; k< a; k++){
-		cout << " " << endl;
-		for (int l = 0; l< b;l++){
-			cout << "| " << matrik[k][l] <<" |";
+	cout<<"Masukkan total data nilai ujian yang ingin dimasukkan: ";cin>>angka;
+	int nilai [angka];
+	for(int i=0; i<angka; i++){
+		cout<<"nilai ke-"<<i<<" = "; cin>>nilai[angka];
 
-			cout<<"Kontol"<<endl;
-		}
+		
 	}
 }
