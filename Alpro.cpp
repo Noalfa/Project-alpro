ca2571c1c3c66b7ddd7f3b09572c6969b6c68df6
Pano <spongab2@gmail.com>
2025-11-21 16:34:50 +0700

    Add matrix input and display in Alpro.cpp
    Implement matrix input and display functionality.

---
+++ Alpro.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+	int a,b;
+	cout << "Masukkan Jumlah Baris Matrik : ";cin >> a;
+	cout << "Masukkan Jumlah Kolom Matrik : ";cin >> b;
+	int matrik[a][b];
+	cout << "Masukkan nilai Matriks"<<endl;
+	for (int i=0;i < a; i++){
+		for (int j=0; j<b; j++){
+			cout <<"["<< i << "] " <<"["<< j<< "] = ";cin >> matrik[i][j];
+		}
+	}
+	cout << endl;
+	cout << "Tampilan Matriks"<<endl;
+	for (int k = 0; k< a; k++){
+		cout << " " << endl;
+		for (int l = 0; l< b;l++){
+			cout << "| " << matrik[k][l] <<" |";
+			
+		}
+	}
+}
--- README.md
+++
@@ -1,2 +0,0 @@
-# Project-alpro
-Alpro
