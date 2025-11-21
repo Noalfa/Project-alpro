043793828ff4b714a11623d6572983228ecb75b1
Rikaruu <raihancoding7812@gmail.com>
2025-11-21 17:23:07 +0700

    Update Alpro.cpp

--- Alpro.cpp
+++ Alpro.cpp
@@ -1,12 +1,35 @@
 #include <iostream>
 using namespace std;
 
-int main(){
-	cout<<"Masukkan total data nilai ujian yang ingin dimasukkan: ";cin>>angka;
-	int nilai [angka];
-	for(int i=0; i<angka; i++){
-		cout<<"nilai ke-"<<i<<" = "; cin>>nilai[angka];
+int main() {
+    int jumlah;
+	int i = 0;
+    cout << "Masukkan jumlah nilai mahasiswa: "; cin >> jumlah;
+    int nilai[jumlah];
 
-		
+	while (i<jumlah) {
+		cout<<"Nilai ke - "<<i+1<<" = "; cin>>nilai[i];
+		i++;
 	}
-}
+
+    int max = nilai[0];
+	i = 1;
+    while (i<jumlah){
+		if (nilai[i] > max) {
+			max = nilai[i];
+		}
+		i++;
+	}
+	int banyak = 0;
+	i = 0;
+	while (i<jumlah) {
+		if(nilai[i] == max){
+			banyak++;
+		}
+		i++;
+	}
+
+    cout<<"Nilai terbesar: " <<max<<endl;
+	cout<<"Banyak mahasiswa memiliki nilai tertinggi = "<<banyak<<endl;
+    
+  }
