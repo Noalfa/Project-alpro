a58d6a3d57d5eedc397fc62db547ee04009853b9
Pano <spongab2@gmail.com>
2025-11-21 17:41:32 +0700

    Update NO4.cpp

--- NO4.cpp
+++ NO4.cpp
@@ -1,1 +1,35 @@
+#include <iostream>
+using namespace std;
+
+int hkm(int bilangan);
+int hm(int bilangan);
+int hcm(int bilangan);
+int hmm(int bilangan);
+
+main(){
+int bilangan;
+cout<<"bilangan = "; cin>>bilangan;
+int km = hkm(bilangan);
+int m = hm(bilangan);
+int cm = hcm(bilangan);
+int mm = hmm(bilangan);
+
+cout << "Hasil Konversi : " << km << " km, "<< m << " m, " << cm << " cm, " << mm << " mm, "<<endl;   
+}
+
+int hkm(int bilangan){
+	return bilangan / 1000000;
+}
+
+int hm(int bilangan){
+	return bilangan % 1000000 / 1000;
+}
+
+int hcm(int bilangan){
+	return bilangan % 1000000 % 1000 / 100;
+}
+
+int hmm(int bilangan){
+	return bilangan % 1000000 % 1000 % 100;
+}
 
