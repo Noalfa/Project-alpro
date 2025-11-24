0e4d679b223a693afa1ed65f4fd0063d28e03aa2
Pano <spongab2@gmail.com>
2025-11-24 16:13:24 +0700

    Update NO6.cpp

--- NO6.cpp
+++ NO6.cpp
@@ -1,1 +1,17 @@
+#include <iostream>
+using namespace std;
+void Konversi(int bilangan,int *jam, int *menit,int *detik);
+int main(){
+	int bilangan, jam, menit, detik;
+	cout<<"bilangan = "; cin>>bilangan;
+	Konversi(bilangan, &jam, &menit, &detik);
+	 cout << bilangan << " detik = " << jam << " jam "<< menit << " menit " << detik << " detik" << endl;
+}
+void Konversi(int bilangan,int *jam, int *menit,int *detik){
+	*jam = bilangan / 3600;
+	bilangan = bilangan % 3600;
+	
+	*menit = bilangan / 60;
+	*detik = bilangan % 60;
+}
 
