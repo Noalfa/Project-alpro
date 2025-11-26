a33b441748af6dbce7a2fc6c0b5606ebb3481b37
Pano <spongab2@gmail.com>
2025-11-26 18:59:34 +0700

    Update NO8.cpp

--- NO8.cpp
+++ NO8.cpp
@@ -2,12 +2,23 @@
 using namespace std;
 
 string tim[13];
+
 void inputTim(){
     cout << "Input Nama Tim:"<<endl;
     for(int i = 0; i < 13; i++){
-        cout << "Masukkan nama tim ke-" << (i + 1) << ": ";getline(cin, timPeserta[i]);
+        cout << "Masukkan nama tim ke-" << i + 1 << ": ";getline(cin, tim[i]);
     }
+    cout << endl;
 }
+
+
+void daftarTim(){
+	for(int i = 0; i < 13; i++){
+		cout << (i + 1) << ". " << tim[i] << endl;
+	}
+	cout << endl;
+}
+
 
 int main() {
     int pilihan;
@@ -18,15 +29,20 @@ int main() {
         cout << "2. Tampilkan Data Tim"<<endl;
         cout << "3. Keluar"<<endl;
         cout << "Pilih menu (1-3): ";cin >> pilihan;
-      
-        if (pilihan == 1) {
-            inputTim();
-        } else if (pilihan == 2) {
-  
-        } else if (pilihan == 3) {
-            cout << "Keluar dari program."<<endl;
-        } else {
-            cout << "Pilihan tidak valid, silakan coba lagi."<<endl;
-        }
+        cin.ignore();
+        switch(pilihan){
+			case 1:
+				inputTim();
+				break;
+			case 2:
+				daftarTim();
+				break;
+			case 3:
+				cout << "Keluar dari program."<<endl;
+				exit(0);
+				break;
+			default:
+				cout << "Pilihan tidak valid, silakan coba lagi."<<endl;
+		}
     } while (pilihan != 3);
 }
