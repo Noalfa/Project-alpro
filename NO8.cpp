d42e18fdeece0717db88ebfb89cf0b48356dc6ba
Pano <spongab2@gmail.com>
2025-11-26 18:44:29 +0700

    Update NO8.cpp

--- NO8.cpp
+++ NO8.cpp
@@ -1,1 +1,22 @@
+int main() {
+    int pilihan;
 
+    do {
+        cout << "=== MENU PENGELOLAAN DATA TIM PIALA DUNIA ==="<<endl;
+        cout << "1. Input Data Tim"<<endl;
+        cout << "2. Tampilkan Data Tim"<<endl;
+        cout << "3. Keluar"<<endl;
+        cout << "Pilih menu (1-3): ";cin >> pilihan;
+      
+        if (pilihan == 1) {
+            inputDataTim();
+            dataSudahDiinput = true;
+        } else if (pilihan == 2) {
+  
+        } else if (pilihan == 3) {
+            cout << "Keluar dari program."<<endl;
+        } else {
+            cout << "Pilihan tidak valid, silakan coba lagi."<<endl;
+        }
+    } while (pilihan != 3);
+}
