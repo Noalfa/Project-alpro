6c9ad09e8ab9ad64b84f5b8ca204d008ab98b196
Rikaruu <raihancoding7812@gmail.com>
2025-11-27 13:22:36 +0700

    Add temperature conversion function stubs
    Added function stubs for temperature conversions.

--- Bebas1.cpp
+++ Bebas1.cpp
@@ -1,6 +1,23 @@
 #include <iostream>
 using namespace std;
 
-int main () {
+double kelvin () {
   
+}
+
+double reamur () {
+  
+}
+
+double fahrenheit () {
+  
+}
+
+int main () {
+  int c = 0;
+  cout<<"Masukkan suhu dalam celcius: "; cin>>c;
+  cout<<endl;
+  
+  cout<<"Menu Konversi Suhu"<<endl;
+  cout<<"1. Fahrenheit "
 }
