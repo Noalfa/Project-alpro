b673a4a0f5c8000b94dacb14432be2fd7e05d39e
Rikaruu <raihancoding7812@gmail.com>
2025-11-27 16:10:02 +0700

    Update Bebas3.cpp

--- Bebas3.cpp
+++ Bebas3.cpp
@@ -1,44 +1,71 @@
 #include <iostream>
 using namespace std;
 
-void penjumlahan () {
-  
+void penjumlahan(float a, float b) {
+  double hasil = a+b;
+  cout << "Hasil penjumlahan: " <<hasil<< endl;
 }
 
-void pengurangan () {
-  
+void pengurangan(float a, float b) {
+  double hasil = a-b;
+  cout << "Hasil pengurangan: " <<hasil<< endl;
 }
 
-void perkalian () {
-  
+void perkalian(float a, float b) {
+  double hasil = a*b;
+  cout << "Hasil perkalian: " <<hasil<< endl;
 }
-void pembagian () {
-  
+
+void pembagian(float a, float b) {
+    if (b != 0) {
+      double hasil = a/b;
+        cout << "Hasil pembagian: " <<hasil<< endl;
+    } else {
+        cout << "Error: pembagian dengan nol tidak diperbolehkan." << endl;
+    }
 }
 
 int main () {
-  float x,y;
-  char ulang;
-  int pilih;
+    float x, y;
+    char ulang;
+    int pilih;
 
-  do {
-  cout<<"===Kalkulator Sederhana==="<<endl;
-  cout<<"1. Penjumlahan "<<endl;
-  cout<<"2. Pengurangan "<<endl;
-  cout<<"3. Perkalian "<<endl;
-  cout<<"4. Pembagian "<<endl;
-  cout<<"5. Keluar "<<endl;
-  cout<<"Pilih menu: "; cin>>pilih;
+    do {
+        cout << "=== Kalkulator Sederhana ===" << endl;
+        cout << "1. Penjumlahan" << endl;
+        cout << "2. Pengurangan" << endl;
+        cout << "3. Perkalian" << endl;
+        cout << "4. Pembagian" << endl;
+        cout << "5. Keluar" << endl;
+        cout << "Pilih menu: ";
+        cin >> pilih;
 
-  cout<<"Masukkan angka pertama: "; cin>>x;
-  cout<<"Masukkan angka kedua: "; cin>>y;
-  
-  switch (pilih) {
-    case 1: {
-      
-    }
-  }
-  
-  }
-  while (ulang == 'y' || ulang == 'Y');
+        cout << "Masukkan angka pertama: ";
+        cin >> x;
+        cout << "Masukkan angka kedua: ";
+        cin >> y;
+
+        switch (pilih) {
+          case 1:
+            penjumlahan(x, y);
+            break;
+          case 2:
+            pengurangan(x, y);
+            break;
+          case 3:
+            perkalian(x, y);
+            break;
+          case 4:
+            pembagian(x, y);
+            break;
+          case 5:
+            cout<<"Keluar dari program..."<<endl;
+            exit(0);
+            break;
+          default:
+            cout << "Pilihan menu tidak tersedia." << endl;
+        }
+        cout << "Ingin mengulang? (y/n): "; cin >> ulang;
+    } while (ulang == 'y' || ulang == 'Y');
+    return 0;
 }
