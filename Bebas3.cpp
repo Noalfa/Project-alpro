657ad74b5af91f8f095da35f10957f144a2441c4
Rikaruu <raihancoding7812@gmail.com>
2025-11-27 15:57:05 +0700

    Update Bebas3.cpp

--- Bebas3.cpp
+++ Bebas3.cpp
@@ -1,6 +1,44 @@
 #include <iostream>
 using namespace std;
 
-int main () {
+void penjumlahan () {
   
+}
+
+void pengurangan () {
+  
+}
+
+void perkalian () {
+  
+}
+void pembagian () {
+  
+}
+
+int main () {
+  float x,y;
+  char ulang;
+  int pilih;
+
+  do {
+  cout<<"===Kalkulator Sederhana==="<<endl;
+  cout<<"1. Penjumlahan "<<endl;
+  cout<<"2. Pengurangan "<<endl;
+  cout<<"3. Perkalian "<<endl;
+  cout<<"4. Pembagian "<<endl;
+  cout<<"5. Keluar "<<endl;
+  cout<<"Pilih menu: "; cin>>pilih;
+
+  cout<<"Masukkan angka pertama: "; cin>>x;
+  cout<<"Masukkan angka kedua: "; cin>>y;
+  
+  switch (pilih) {
+    case 1: {
+      
+    }
+  }
+  
+  }
+  while (ulang == 'y' || ulang == 'Y');
 }
