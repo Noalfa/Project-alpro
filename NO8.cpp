9c58f705c60041ec2c40fa25f99cbede4afb6177
Pano <spongab2@gmail.com>
2025-11-27 21:49:58 +0700

    Update NO8.cpp

--- NO8.cpp
+++ NO8.cpp
@@ -2,62 +2,104 @@
 using namespace std;
 
 string tim[13];
-string bagi[4][3];
-int groupSize[4] = {4, 3, 3, 3};
-bool sbagi = true;
-
+string bagi[4][4];
+int grup[4] = {4, 3, 3, 3};
+int banyaktim = 0;
+bool ada = false;
 
 void inputTim(){
-    cout << "Input Nama Tim:"<<endl;
-    for(int i = 0; i < 13; i++){
+	int n;
+    cout << "Berapa jumlah tim yang akan diinput (maks 13)? ";cin >> n;
+    cin.ignore();
+    if(n < 1 || n > 13){
+		cout << "Minimal 1 tim dan maksimal 13 tim"<<endl;
+		return;
+	}
+    for(int i = 0; i < n; i++){
         cout << "Masukkan nama tim ke-" << i + 1 << ": ";getline(cin, tim[i]);
     }
+    for(int i = n; i < 13; i++) tim[i] = "";
+    banyaktim = n;
+    ada = (banyaktim > 0);
     cout << endl;
 }
 
 
 void daftarTim(){
-	cout << endl;
-	for(int i = 0; i < 13; i++){
-		cout << (i + 1) << ". " << tim[i] << endl;
+	if(!ada){
+		cout << "Belum ada data tim. Silahkan input data tim"<< endl;
+		return;
+	}else{
+		cout << endl;
+		for(int i = 0; i < 13; i++){
+			cout << (i + 1) << ". " << tim[i] << endl;
+		}
 	}
 	cout << endl;
 }
 
 
 void bagiTim(){
-	int idx = 0;
-    // Grup A 4 tim
-    for(int t = 0; t < 4; t++){
-        bagi[0][t] = tim[idx++];
-    }
-    // Grup B, C, D masing-masing 3 tim
-    for(int g = 1; g < 4; g++){
-        for(int t = 0; t < 3; t++){
-            bagi[g][t] = tim[idx++];
-        }
-    }
+	if(!ada){
+		cout << "Belum ada data tim. Silahkan input data tim"<< endl;
+		return;
+	}else{
+		int id = 0;
+		for(int t = 0; t < 4; t++){
+			bagi[0][t] = tim[id++];
+		}
     
-	cout << "=== HASIL PEMBAGIAN GRUP ===\n";
-    for(int g = 0; g < 4; g++){
-        cout << "Grup " << char('A' + g) << " (" << groupSize[g] << " tim): ";
-        for(int t = 0; t < groupSize[g]; t++){
-            cout << bagi[g][t];
-            if(t < groupSize[g] - 1) cout << ", ";
-        }
-        cout << endl;
-    }
-    cout << endl;
-    sbagi = true;
+		for(int g = 1; g < 4; g++){
+			for(int t = 0; t < 3; t++){
+				bagi[g][t] = tim[id++];
+			}
+		}
+    
+		cout << "=== HASIL PEMBAGIAN GRUP ===\n";
+		for(int g = 0; g < 4; g++){
+			cout << "Grup " << char('A' + g) << " (" << grup[g] << " tim): ";
+			for(int t = 0; t < grup[g]; t++){
+				cout << bagi[g][t];
+				if(t < grup[g] - 1) cout << ", ";
+			}	
+			cout << endl;
+		}
+	}
+	cout << endl;
 }
 
-
-void pemenangGroup(){
-	if(!sbagi){
-		cout << "Belum ada pembagian grup."<<endl;
+void ubahData(){
+	int pilih, id;
+	string namaBaru;
+	if(!ada){
+		cout << "Belum ada data tim. Silahkan input data tim"<< endl;
 		return;
 	}
-	
+	daftarTim();
+	cout << "Pilih nomor tim yang ingin diubah/hapus: ";cin >> id;
+	cin.ignore();
+    if(id < 1 || id > banyaktim){
+        cout << "Nomor tim tidak valid.\n\n";
+        return;
+    }
+    cout << "1. Ubah nama tim"<< endl;
+    cout << "2. Hapus tim"<< endl;
+    cout << "Pilih (1-2): ";cin >> pilih;
+    if(pilih == 1){
+        cout << "Masukkan nama baru untuk tim ke-" << id << ": ";getline(cin, namaBaru);
+        tim[id - 1] = namaBaru;
+        cout << "Nama tim berhasil diubah.\n\n";
+    } else if(pilih == 2){
+        for(int i = id - 1; i < banyaktim - 1; i++){
+            tim[i] = tim[i + 1];
+        }
+        tim[banyaktim - 1] = "";
+        banyaktim--;
+        if(banyaktim == 0) ada = false;
+        cout << "Tim berhasil dihapus.\n\n";
+    } else {
+        cout << "Aksi tidak valid.\n\n";
+    }
 }
 
 int main() {
@@ -68,8 +110,7 @@ int main() {
         cout << "1. Input Data Tim"<<endl;
         cout << "2. Tampilkan Data Tim"<<endl;
         cout << "3. Bagi Otomatis (A=4, B=3, C=3, D=3)"<<endl;
-        cout << "4. Pilih Pemenang Tiap Grup"<<endl;
-        cout << "5. Pilih Juara 1-3" << endl;
+        cout << "4. Ubah Data Tim"<<endl;
         cout << "Pilih menu (1-5): ";cin >> pilihan;
         cin.ignore();
         switch(pilihan){
@@ -77,21 +118,14 @@ int main() {
 				inputTim();
 				break;
 			case 2:
-				if(tim[0].empty()){
-					cout << "Belum ada data tim. Silahkan input data tim"<< endl;
-				}else{
-					daftarTim();
-				}
+				daftarTim();
 				break;
 			case 3:
 				bagiTim();
 				break;
 			case 4:
-				pemenangGrup();
+				ubahData();
 				break;	
-			case 5:
-				
-				break;
 			default:
 				cout << "Pilihan tidak valid, silakan coba lagi."<<endl;
 		}
